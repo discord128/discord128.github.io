0c82b8e2f1a53f142e2f8a5bb94652e17276ebd1
discord128 <60793651+discord128@users.noreply.github.com>
2020-02-07 10:48:40 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Discord for Chrome",
+   "version": "0.0.0.4",
+   "homepage": "https://discordapp.com/channels/@me",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
